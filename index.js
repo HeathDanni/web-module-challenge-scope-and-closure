63f2bcb85ce493b486c0ce47a40c579dbf1a35cc
Heather Beckman <heatherbeckman85@gmail.com>
2020-07-08 23:45:11 +0000

    Task 2

--- index.js
+++ index.js
@@ -14,9 +14,9 @@
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
 */
-function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
-}
+// function processFirstItem(stringList, callback) {
+//   return callback(stringList[0])
+// }
 
 // ⭐️ Example Challenge END ⭐️
 
@@ -27,40 +27,49 @@ function processFirstItem(stringList, callback) {
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
  * 1. What is the difference between counter1 and counter2?
+ *      
+ * In counter1, count is declared and initialized inside the function, so it will reset each time the function is run.  In counter2, count is declared and initialized outside the function so it will not reset and will increment when the function is run multiple times.
  * 
  * 2. Which of the two uses a closure? How can you tell?
  * 
+ * counter2 uses a closure because count is declared and initialized outside of the function brackets so it will not reset.
+ * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
+ * counter one would be preferable if you want to reset count each time
  *
 */
 
 // counter1 code
-function counterMaker() {
-  let count = 0;
-  return function counter() {
-   return count++;
-  }
-}
+// function counterMaker() {
+//   let count = 0;
+//   return function counter() {
+//    return count++;
+//   }
+// }
 
-const counter1 = counterMaker();
+// console.log(counter());
 
-// counter2 code
-let count = 0;
+// const counter1 = counterMaker();
 
-function counter2() {
-  return count++;
-}
+// // counter2 code
+// let count = 0;
+
+// function counter2() {
+//   return count++;
+// }
 
 
 /* Task 2: inning() 
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
+function inning(){
 
-    /*Code Here*/
+return Math.floor(Math.random() * 3);
 
 }
+
+// console.log(inning());
 
 /* Task 3: finalScore()
 
